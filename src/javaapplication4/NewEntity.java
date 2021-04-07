ab57be38d7302c6da4182999ac2c9462dc52c420
18744 <18744@1K148s2.dune.edu>
2021-04-07 14:11:23 +0200

    feat: edit file Studenty.xml

--- src/javaapplication4/NewEntity.java
+++ src/javaapplication4/NewEntity.java
@@ -14,6 +14,8 @@ import javax.persistence.Id;
 import javax.persistence.Query;
 
 
+
+
 @Entity
 public class NewEntity implements Serializable {
     private static final long serialVersionUID = 1L;
