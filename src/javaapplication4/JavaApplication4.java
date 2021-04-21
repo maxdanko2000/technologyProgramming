724157c30038b5576a76b6eed19a064d9fc206a2
18744 <18744@1k122s14.dune.edu>
2021-04-21 14:38:20 +0200

    feat: comment

--- src/javaapplication4/JavaApplication4.java
+++ src/javaapplication4/JavaApplication4.java
@@ -1,9 +1,28 @@
 package javaapplication4;
 
-
-
+import entity.Studentyi;
+import java.util.List;
+import javax.persistence.Query;
+import utils.NewHibernateUtil;
+import org.hibernate.Session;
 public class JavaApplication4 {
   
-   
+   public static void main(String[] args){
+      Session session = NewHibernateUtil.getSessionFactory().openSession();
+      session.beginTransaction();
+      String hql = "from Studentyi a where s.gruppyi.shifr = :param";
+      Query query = (Query) session.createQuery(hql);
+      
+      List <Studentyi> rows;
+      
+       rows = query.list();
+      
+       
+       
+       rows.forEach((row) -> {
+           System.out.println(row.getImya() + " " + row.getFamiliya() + " " + row.getOtchestvo());
+       });
+      
+   }
    
 }
