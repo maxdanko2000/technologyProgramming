baf2887edb238cf332547d48ae0ec8f415c9b2b0
18744 <18744@1k122s14.dune.edu>
2021-04-21 15:00:24 +0200

    feat: edit

--- src/javaapplication4/JavaApplication4.java
+++ src/javaapplication4/JavaApplication4.java
@@ -1,5 +1,6 @@
 package javaapplication4;
 
+import entity.Gruppyi;
 import entity.Studentyi;
 import java.util.List;
 import javax.persistence.Query;
@@ -11,14 +12,20 @@ public class JavaApplication4 {
       Session session = NewHibernateUtil.getSessionFactory().openSession();
       session.beginTransaction();
       String hql = "from Studentyi a where s.gruppyi.shifr = :param";
+      String hql1 = "from Gruppyi a";
       Query query = (Query) session.createQuery(hql);
-      
-      List <Studentyi> rows = query.getResultList();
-    
+      Query query1 = (Query) session.createQuery(hql);
+      List <Studentyi> rows;
+       rows = query.list();
+      List <Gruppyi> rows1;
+       rows1 = query1.list();
+       
        rows.forEach((row) -> {
            System.out.println(row.getImya() + " " + row.getFamiliya() + " " + row.getOtchestvo());
        });
-      
+       rows1.forEach((row) -> {
+           System.out.println(row.getNazvanie());
+       });
    }
    
 }
